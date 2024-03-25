cbba3d82a8919f75005d8911fa30ceb064f3ad63
nguyentanhueqn123 <nguyentanhuebc@gmail.com>
2024-03-25 11:22:45 +0700

    feat: download file excel

--- src/components/TableRowExpandCollapse/MealDetailTable.tsx
+++ src/components/TableRowExpandCollapse/MealDetailTable.tsx
@@ -66,55 +66,179 @@ const TableComponent: React.FC<{ data: Meal[] }> = ({ data }) => {
   };
 
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th rowSpan={2}></th>
-          <th rowSpan={2}>Date</th>
-          <th rowSpan={2}>RVC</th>
-          <th rowSpan={2}>Period</th>
-          {/* <th colSpan={4}>Expected</th>
+    <>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th rowSpan={2}></th>
+            <th rowSpan={2}>Date</th>
+            <th rowSpan={2}>RVC</th>
+            <th rowSpan={2}>Period</th>
+            {/* <th colSpan={4}>Expected</th>
           <th colSpan={4}>Actual</th>
           <th colSpan={2}>Count (%)</th>
           <th colSpan={2}>Sales (%)</th> */}
-        </tr>
-        <tr>
-          <th>A. Count</th>
-          <th>C. Count</th>
-          <th>A. Sales</th>
-          <th>C. Sales</th>
-          <th>Count (%)</th>
-          <th>Sales (%)</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((meal) => (
-          <React.Fragment key={meal.report_date}>
-            <tr>
-              <td onClick={() => handleRowClick(meal.report_date)}>
-                {expandedRows[meal.report_date] ? <FiMinus /> : <FiPlus />}
-              </td>
-              <td>{meal.report_date}</td>
-              <td colSpan={2}></td>
-              <td>{meal.total.total_actual.count}</td>
-              <td>{meal.total.children_actual.count}</td>
-              <td>{meal.total.total_actual.sales}</td>
-              <td>{meal.total.children_actual.sales}</td>
-              <td>{meal.total.total_actual.percentage_count}</td>
-              <td>{meal.total.total_actual.percentage_sales}</td>
-            </tr>
-            {expandedRows[meal.report_date] &&
-              meal.outlet.map((outlet, index) => (
+          </tr>
+          <tr>
+            <th>A. Count</th>
+            <th>C. Count</th>
+            <th>A. Sales</th>
+            <th>C. Sales</th>
+            <th>Count (%)</th>
+            <th>Sales (%)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((meal) => (
+            <React.Fragment key={meal.report_date}>
+              <tr>
+                <td onClick={() => handleRowClick(meal.report_date)}>
+                  {expandedRows[meal.report_date] ? <FiMinus /> : <FiPlus />}
+                </td>
+                <td>{meal.report_date}</td>
+                <td></td>
+                <td></td>
+
+                <td>{meal.total.total_actual.count}</td>
+                <td>{meal.total.children_actual.count}</td>
+                <td>{meal.total.total_actual.sales}</td>
+                <td>{meal.total.children_actual.sales}</td>
+                <td>{meal.total.total_actual.percentage_count}</td>
+                <td>{meal.total.total_actual.percentage_sales}</td>
+              </tr>
+              {expandedRows[meal.report_date] &&
+                meal.outlet.map((outlet, index) => (
+                  <React.Fragment key={outlet.outlet_code}>
+                    <tr>
+                      <td></td>
+                      <td onClick={() => handleRowClick(outlet.outlet_code)}>
+                        {expandedRows[outlet.outlet_code] ? (
+                          <FiMinus />
+                        ) : (
+                          <FiPlus />
+                        )}
+                      </td>
+                      <td>{outlet.outlet_code}</td>
+                      <td></td>
+                      <td>{outlet.total.total_actual.count}</td>{" "}
+                      <td>{outlet.total.children_actual.count}</td>{" "}
+                      <td>{outlet.total.total_actual.sales}</td>{" "}
+                      <td>{outlet.total.children_actual.sales}</td>{" "}
+                      <td>{outlet.total.total_actual.percentage_count}</td>{" "}
+                      <td>{outlet.total.total_actual.percentage_sales}</td>{" "}
+                    </tr>
+                    {expandedRows[outlet.outlet_code] && (
+                      <>
+                        <tr>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+
+                          <td>Breakfast</td>
+                          <td>{outlet.breakfast.total.total_actual.count}</td>
+                          <td>
+                            {outlet.breakfast.total.children_actual.count}
+                          </td>
+                          <td>{outlet.breakfast.total.total_actual.sales}</td>
+                          <td>
+                            {outlet.breakfast.total.children_actual.sales}
+                          </td>
+                          <td>
+                            {
+                              outlet.breakfast.total.total_actual
+                                .percentage_count
+                            }
+                          </td>
+                          <td>
+                            {
+                              outlet.breakfast.total.total_actual
+                                .percentage_sales
+                            }
+                          </td>
+                        </tr>
+                        <tr>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+
+                          <td>Lunch</td>
+                          <td>{outlet.lunch.total.total_actual.count}</td>
+                          <td>{outlet.lunch.total.children_actual.count}</td>
+                          <td>{outlet.lunch.total.total_actual.sales}</td>
+                          <td>{outlet.lunch.total.children_actual.sales}</td>
+                          <td>
+                            {outlet.lunch.total.total_actual.percentage_count}
+                          </td>
+                          <td>
+                            {outlet.lunch.total.total_actual.percentage_sales}
+                          </td>
+                        </tr>
+                        <tr>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td>Dinner</td>
+                          <td>{outlet.dinner.total.total_actual.count}</td>
+                          <td>{outlet.dinner.total.children_actual.count}</td>
+                          <td>{outlet.dinner.total.total_actual.sales}</td>
+                          <td>{outlet.dinner.total.children_actual.sales}</td>
+                          <td>
+                            {outlet.dinner.total.total_actual.percentage_count}
+                          </td>
+                          <td>
+                            {outlet.dinner.total.total_actual.percentage_sales}
+                          </td>
+                        </tr>
+                      </>
+                    )}
+                  </React.Fragment>
+                ))}
+            </React.Fragment>
+          ))}
+        </tbody>
+      </Table>
+      <Table striped bordered hover className="table-v2">
+        <thead>
+          <tr>
+            <th rowSpan={2}></th>
+            <th rowSpan={2}>Date</th>
+            <th rowSpan={2}>RVC</th>
+            <th rowSpan={2}>Period</th>
+            {/* <th colSpan={4}>Expected</th>
+          <th colSpan={4}>Actual</th>
+          <th colSpan={2}>Count (%)</th>
+          <th colSpan={2}>Sales (%)</th> */}
+          </tr>
+          <tr>
+            <th>A. Count</th>
+            <th>C. Count</th>
+            <th>A. Sales</th>
+            <th>C. Sales</th>
+            <th>Count (%)</th>
+            <th>Sales (%)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((meal) => (
+            <React.Fragment key={meal.report_date}>
+              <tr>
+                <td></td>
+                <td>{meal.report_date}</td>
+                <td></td>
+                <td></td>
+
+                <td>{meal.total.total_actual.count}</td>
+                <td>{meal.total.children_actual.count}</td>
+                <td>{meal.total.total_actual.sales}</td>
+                <td>{meal.total.children_actual.sales}</td>
+                <td>{meal.total.total_actual.percentage_count}</td>
+                <td>{meal.total.total_actual.percentage_sales}</td>
+              </tr>
+              {meal.outlet.map((outlet, index) => (
                 <React.Fragment key={outlet.outlet_code}>
                   <tr>
                     <td></td>
-                    <td onClick={() => handleRowClick(outlet.outlet_code)}>
-                      {expandedRows[outlet.outlet_code] ? (
-                        <FiMinus />
-                      ) : (
-                        <FiPlus />
-                      )}
-                    </td>
+                    <td></td>
                     <td>{outlet.outlet_code}</td>
                     <td></td>
                     <td>{outlet.total.total_actual.count}</td>{" "}
@@ -124,58 +248,65 @@ const TableComponent: React.FC<{ data: Meal[] }> = ({ data }) => {
                     <td>{outlet.total.total_actual.percentage_count}</td>{" "}
                     <td>{outlet.total.total_actual.percentage_sales}</td>{" "}
                   </tr>
-                  {expandedRows[outlet.outlet_code] && (
-                    <>
-                      <tr>
-                        <td colSpan={3}></td>
-                        <td>Breakfast</td>
-                        <td>{outlet.breakfast.total.total_actual.count}</td>
-                        <td>{outlet.breakfast.total.children_actual.count}</td>
-                        <td>{outlet.breakfast.total.total_actual.sales}</td>
-                        <td>{outlet.breakfast.total.children_actual.sales}</td>
-                        <td>
-                          {outlet.breakfast.total.total_actual.percentage_count}
-                        </td>
-                        <td>
-                          {outlet.breakfast.total.total_actual.percentage_sales}
-                        </td>
-                      </tr>
-                      <tr>
-                        <td colSpan={3}></td>
-                        <td>Lunch</td>
-                        <td>{outlet.lunch.total.total_actual.count}</td>
-                        <td>{outlet.lunch.total.children_actual.count}</td>
-                        <td>{outlet.lunch.total.total_actual.sales}</td>
-                        <td>{outlet.lunch.total.children_actual.sales}</td>
-                        <td>
-                          {outlet.lunch.total.total_actual.percentage_count}
-                        </td>
-                        <td>
-                          {outlet.lunch.total.total_actual.percentage_sales}
-                        </td>
-                      </tr>
-                      <tr>
-                        <td colSpan={3}></td>
-                        <td>Dinner</td>
-                        <td>{outlet.dinner.total.total_actual.count}</td>
-                        <td>{outlet.dinner.total.children_actual.count}</td>
-                        <td>{outlet.dinner.total.total_actual.sales}</td>
-                        <td>{outlet.dinner.total.children_actual.sales}</td>
-                        <td>
-                          {outlet.dinner.total.total_actual.percentage_count}
-                        </td>
-                        <td>
-                          {outlet.dinner.total.total_actual.percentage_sales}
-                        </td>
-                      </tr>
-                    </>
-                  )}
+                  <>
+                    <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+
+                      <td>Breakfast</td>
+                      <td>{outlet.breakfast.total.total_actual.count}</td>
+                      <td>{outlet.breakfast.total.children_actual.count}</td>
+                      <td>{outlet.breakfast.total.total_actual.sales}</td>
+                      <td>{outlet.breakfast.total.children_actual.sales}</td>
+                      <td>
+                        {outlet.breakfast.total.total_actual.percentage_count}
+                      </td>
+                      <td>
+                        {outlet.breakfast.total.total_actual.percentage_sales}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+
+                      <td>Lunch</td>
+                      <td>{outlet.lunch.total.total_actual.count}</td>
+                      <td>{outlet.lunch.total.children_actual.count}</td>
+                      <td>{outlet.lunch.total.total_actual.sales}</td>
+                      <td>{outlet.lunch.total.children_actual.sales}</td>
+                      <td>
+                        {outlet.lunch.total.total_actual.percentage_count}
+                      </td>
+                      <td>
+                        {outlet.lunch.total.total_actual.percentage_sales}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td>Dinner</td>
+                      <td>{outlet.dinner.total.total_actual.count}</td>
+                      <td>{outlet.dinner.total.children_actual.count}</td>
+                      <td>{outlet.dinner.total.total_actual.sales}</td>
+                      <td>{outlet.dinner.total.children_actual.sales}</td>
+                      <td>
+                        {outlet.dinner.total.total_actual.percentage_count}
+                      </td>
+                      <td>
+                        {outlet.dinner.total.total_actual.percentage_sales}
+                      </td>
+                    </tr>
+                  </>
                 </React.Fragment>
               ))}
-          </React.Fragment>
-        ))}
-      </tbody>
-    </Table>
+            </React.Fragment>
+          ))}
+        </tbody>
+      </Table>
+    </>
   );
 };
 
--- src/pages/PeriodDetail/PeriodDetail.tsx
+++ src/pages/PeriodDetail/PeriodDetail.tsx
@@ -1,19 +1,41 @@
 import React from "react";
 import AdminContainer from "../../components/AdminContainer/AdminContainer";
-import { IonContent } from "@ionic/react";
+import { IonContent, IonButton, IonIcon } from "@ionic/react";
 import { useSelector } from "react-redux";
 import MealDetailTable from "../../components/TableRowExpandCollapse/MealDetailTable";
+import * as XLSX from "xlsx";
+import { downloadOutline } from "ionicons/icons";
 
 const PeriodDetail: React.FC = () => {
   const periodDetail = useSelector((state: any) => state.periodDetail);
+  console.log(periodDetail);
+
+  // const handleDownload = () => {
+  //   const ws = XLSX.utils.table_to_sheet(document.querySelector("table"));
+  //   const wb = XLSX.utils.book_new();
+  //   XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
+  //   XLSX.writeFile(wb, "meal_detail_table.xlsx");
+  // };
+  const handleDownload = () => {
+    const wb = XLSX.utils.book_new();
+    const ws = XLSX.utils.table_to_sheet(document.querySelector(".table-v2"));
+    XLSX.utils.book_append_sheet(wb, ws, "MealDetailTable");
+    XLSX.writeFile(wb, "MealDetailTable.xlsx");
+  };
 
   return (
     <IonContent>
       <AdminContainer>
         <h4 className="font-bold my-2">Period Detail</h4>
-        <p className="text-center bg-[#62b4ff] text-white px-3 py-2 mt-4">
+        <IonButton className="mt-4" color="secondary" onClick={handleDownload}>
+          <IonIcon slot="start" icon={downloadOutline}></IonIcon>
+          Download Excel
+        </IonButton>
+
+        <p className="text-center bg-[#62b4ff] text-white px-3 py-2 mt-2">
           Expected and Actual Meal Count and Sales
         </p>
+
         <div className="overflow-x-auto">
           <MealDetailTable data={periodDetail} />
         </div>
